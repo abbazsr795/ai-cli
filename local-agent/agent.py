eefccf4df23f65e9145ba0e63fe3eaf08e23e127
abbazsr795 <abbazsr795@gmail.com>
2025-11-18 14:54:37 +1100

    fix : some of the tools are being used

--- local-agent/agent.py
+++ local-agent/agent.py
@@ -3,7 +3,7 @@ from tools.filesystem import search_files, fetch_files, get_current_dir
 from gpt4all import GPT4All
 
 model = GPT4All("gpt4all-13b-snoozy-q4_0.gguf")
-memory = Memory()
+memory = Memory(20)
 
 def call_llm(prompt):
     output = model.generate(prompt)
@@ -14,15 +14,21 @@ def decide_and_act(user_input):
     memory.add("user", user_input)
 
     system_prompt = """
-You are a local AI agent.
+You are a local AI agent that runs on command line.
+
 You can either:
 - Answer normally, OR
-- Request a tool call using format: TOOL: filesystem.tool_name()
+- Request a tool
+
+To request a tool, respond with one of the following. do not add/delete/modify to response:
+- TOOL:filesystem.fetchFiles()
+- TOOL:filesystem.getCurrentDir()
+
+NEVER guess a response 
 
 Available tools:
-- filesystem.search_files(pattern)
-- filesystem.fetch_files()
-- filesystem.get_current_dir()
+- filesystem.fetchFiles() : if user requests a list of all files in current directory. 
+- filesystem.getCurrentDir() : if user requests the current directory
 """
 
     full_prompt = system_prompt
@@ -31,20 +37,21 @@ Available tools:
 
     full_prompt += "\nassistant:"
 
-    response = call_llm(full_prompt)
+    response = call_llm(full_prompt).strip()
+    print("xxx" + response + "xxx")
 
     if response.startswith("TOOL:"):
         inside = response[5:].strip()
-        if "filesystem.search_files" in inside:
-            pattern = inside.replace("filesystem.search_files(", "").replace(")", "")
-            result = search_files(pattern)
-            memory.add("assistant", str(result))
-            return result
-        if "filesystem.fetch_files" in inside:
+        # if "filesystem.search_files" in inside:
+        #     pattern = inside.replace("filesystem.search_files(", "").replace(")", "")
+        #     result = search_files(pattern)
+        #     memory.add("assistant", str(result))
+        #     return result
+        if "filesystem.fetchFiles" in inside:
             result = fetch_files()
             memory.add("assistant", str(result))
             return result
-        if "filesystem.get_current_dir" in inside:
+        if "filesystem.getCurrentDir" in inside:
             result = get_current_dir()
             memory.add("assistant", str(result))
             return result
